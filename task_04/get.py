fc2d6f4f896da4276375ab79169d721930c6ba73
Dmitry Aseev <nemnem@rambler.ru>
2024-07-25 21:02:22 +0300

    Add task_04

---
+++ task_04/get.py
@@ -0,0 +1,75 @@
+import requests
+import asyncio
+import logging
+import time
+from threading import Thread
+from multiprocessing import Process
+from aiohttp import ClientSession
+from pathlib import Path, PurePath
+
+
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+img_dir = Path('./imgs')
+
+
+def download_img(url: str):
+    start_time = time.time()
+    if not img_dir.exists():
+        img_dir.mkdir(parents=True)
+    response = requests.get(url)
+    filename = img_dir.joinpath(PurePath(url.split('/')[-1]))
+    with open(filename, "wb") as f:
+        f.write(response.content)
+    logger.info(f'Download {filename} completed in {(time.time() - start_time):.2f} seconds')
+
+
+async def download_async_img(url: str):
+    start_time = time.time()
+    if not img_dir.exists():
+        img_dir.mkdir(parents=True)
+    async with ClientSession() as session:
+        async with session.get(url) as response:
+            content = await response.read()
+            filename = img_dir.joinpath(PurePath(url.split('/')[-1]))
+            with open(filename, "wb") as f:
+                f.write(content)
+            logger.info(f'Download {filename} completed in {(time.time() - start_time):.2f} seconds')
+
+
+def created_thread(urls: list):
+    
+    start_time = time.time()
+    threads = []
+    for url in urls:
+        thread = Thread(target=download_img, args=[url])
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+    logger.info(f'Task completed in {(time.time() - start_time):.2f} seconds')
+
+
+def created_multi(urls: list):
+
+    start_time = time.time()
+    processes = []
+    for url in urls:
+        process = Process(target=download_img, args=(url,))
+        processes.append(process)
+        process.start()
+    for process in processes:
+        process.join()
+    logger.info(f'Task completed in {(time.time() - start_time):.2f} seconds')
+    
+
+async def created_async(urls: list):
+    start_time = time.time()
+    tasks = []
+    for url in urls:
+        task = asyncio.ensure_future(download_async_img(url))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
+    logger.info(f'Task completed in {(time.time() - start_time):.2f} seconds')  
---
+++ task_04/main.py
@@ -0,0 +1,33 @@
+import argparse
+import asyncio
+from get import created_thread, created_multi, created_async
+
+
+test_urls = ['https://www.firefallphotography.com/wp-content/uploads/2018/11/2018-Bisti-10-08-08449.jpg',
+            'https://www.fabsite.org/misc/bsd-daemonette/Ceren-Ercen.jpg',
+            'http://4everstatic.com/pictures/850xX/art/movies/bender-bending-rodriguez,-futurama-159767.jpg',
+            'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png',
+            'https://content.eveonline.com/www/newssystem/media/67273/1/Vargur_Thumb.jpg'
+            ]
+
+
+def parse():
+    
+    parser = argparse.ArgumentParser(description='Загрузка изображений с сайта')
+    parser.add_argument('urls', help='url for download', nargs='*', default=test_urls)
+    args = parser.parse_args()
+
+    urls = args.urls
+    
+    print(f'Загрузка с помощью threading.')
+    created_thread(urls)
+
+    print(f'Загрузка с помощью multiprocessing')
+    created_multi(urls)
+
+    print(f'Загрузка с помощью asyncio')
+    asyncio.run(created_async(urls))
+
+
+if __name__ == "__main__":
+    parse()
